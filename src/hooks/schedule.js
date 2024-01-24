6c5375d2b6ae2c04ec2a10861367ca8d01a50ab2
Rohit <rohitadinathsalgar@dokonaly.com>
2024-01-24 10:41:02 +0530

    schedule add

--- src/hooks/schedule.js
+++ src/hooks/schedule.js
@@ -1,14 +1,34 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { fetchData } from "../helper";
 import { URL } from "../config";
 import { toast } from "react-toastify";
 
 const useAddSchedule = () => {
+    const queryclient = useQueryClient();
     return useMutation({
         mutationFn: (data) =>
             fetchData(
                 {
-                    url: URL + "users/updateUserDetails",
+                    url: URL + "schedule/addSchedule",
+                    method: "POST",
+                    isAuthRequired: true,
+                },
+                { data: [data] }
+            ),
+        onSuccess: () => {
+            queryclient.invalidateQueries(['schedules'])
+        },
+        onError: (error) => {
+            toast.error(error.message.split(":")[1]);
+        },
+    });
+};
+const useDeleteSchedule = () => {
+    return useMutation({
+        mutationFn: (data) =>
+            fetchData(
+                {
+                    url: URL + "schedule/deleteSchedule",
                     method: "POST",
                     isAuthRequired: true,
                 },
@@ -21,4 +41,22 @@ const useAddSchedule = () => {
 };
 
 
-export { useAddSchedule }
+const useGetAllMySchedules = (id) => {
+    return useQuery({
+        queryKey: ['schedules', id],
+        queryFn: () => {
+            return fetchData({
+                url: URL + "schedule/getMySchedule",
+                method: "POST",
+                isAuthRequired: true
+            }, {
+                data: [{ companyId: id }]
+            },
+
+            )
+
+        },
+    })
+}
+
+export { useAddSchedule, useDeleteSchedule, useGetAllMySchedules }
