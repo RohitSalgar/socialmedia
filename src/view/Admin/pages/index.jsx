d71c74e381b29561ee335d24f019478a397e478a
Balreddy mahendra <balreddy.mahendra@dokonaly.com>
2024-01-27 01:24:09 +0530

    completed page management for admin

--- src/view/Admin/pages/index.jsx
+++ src/view/Admin/pages/index.jsx
@@ -1,35 +1,54 @@
+import { useState } from "react";
 import classes from "../posts/index.module.css";
-import { IconButton, InputBase, Typography } from "@mui/material";
+import { IconButton, Typography } from "@mui/material";
 import { Search } from "@mui/icons-material";
 import { DataGrid } from "@mui/x-data-grid";
 import { useTheme } from "@emotion/react";
 import Button from "@mui/material/Button";
+import { useGetAllUnverifiedPages, useVerifyPage } from "../../../hooks/admin";
+import Loader from "../../../components/Loader/Loader";
+import moment from "moment";
 
 const pages = () => {
   const { palette } = useTheme();
   const primary = palette.primary.main;
+  const {data: pagesData, isLoading} = useGetAllUnverifiedPages()
+  const {mutate, isPaused} = useVerifyPage()
+  const [searchTerm, setSearchTerm] = useState("");
+
   const columns = [
     {
-      field: "createdBy",
-      headerName: "Created User",
-      flex: 1,
-      headerAlign: "center",
-      align: "center",
-      headerClassName: "tabel-header",
-    },
-    {
-      field: "description",
-      headerName: "Description",
+      field: "companyName",
+      headerName: "Company Name",
       flex: 1.5,
       headerAlign: "center",
       align: "center",
       headerClassName: "tabel-header",
     },
     {
-      field: "reportCount",
-      headerAlign: "center",
-      headerName: "Reported Users",
+      field: "email",
+      headerName: "Email",
       flex: 1,
+      headerAlign: "center",
+      align: "center",
+      headerClassName: "tabel-header",
+    },
+    {
+      field: "about",
+      headerName: "About",
+      flex: 1.5,
+      headerAlign: "center",
+      align: "center",
+      headerClassName: "tabel-header",
+      valueGetter: ({ value }) => value ?? "-",
+    },
+    {
+      field: "licenseNo",
+      headerName: "License No",
+      flex: 1.5,
+      headerAlign: "center",
+      align: "center",
+      headerClassName: "tabel-header",
     },
     {
       field: "createdAt",
@@ -38,6 +57,7 @@ const pages = () => {
       headerAlign: "center",
       align: "center",
       headerClassName: "tabel-header",
+      valueGetter: ({ value }) => moment(value).format("DD-MM-YYYY"),
     },
     {
       field: "Options",
@@ -46,25 +66,23 @@ const pages = () => {
       headerAlign: "center",
       align: "center",
       headerClassName: "tabel-header",
-      renderCell: ({ row }) => <Button variant="contained">Delete</Button>,
+      renderCell: ({ row }) => 
+      <>
+      <Button onClick={() => mutate({id: row._id, status: 1})} variant="contained">Approve</Button>
+      <Button onClick={() => mutate({id: row._id, status: 4})} variant="outlined">Reject</Button>
+      </>,
     },
   ];
 
-  const rows = [
-    {
-      _id: 1,
-      createdBy: "Mahendra",
-      description: "sdfsdfsdf sadfasdf sdfadfa awdfadfasdf",
-      reportCount: [{ name: "mahi" }, { name: "balue" }],
-      createdAt: "11/01/2024",
-    },
-  ];
+  if(isLoading || isPaused) {
+    return <Loader />
+  }
 
   return (
     <section className={classes.postSection}>
       <div>
         <Typography variant="h2" color={primary}>
-          Pages
+          Company Pages List
         </Typography>
       </div>
       <div className={classes.searchContainer}>
@@ -72,15 +90,17 @@ const pages = () => {
           <Search />
         </IconButton>
         <input
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
           className={classes.searchInput}
-          placeholder="Search by username..."
+          placeholder="Search by company name..."
         />
       </div>
       <div>
         <DataGrid
           sx={{ textTransform: "capitalize", minHeight: "450px" }}
           getRowId={(row) => row._id}
-          rows={rows}
+          rows={pagesData.filter(page => page.companyName.toLowerCase().includes(searchTerm.toLowerCase()))}
           columns={columns}
           initialState={{
             pagination: {
