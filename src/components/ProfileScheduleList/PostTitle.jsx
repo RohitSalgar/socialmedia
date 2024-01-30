5c895a6e8b48130dcbc2b9a0be893b8917884085
Rohit <rohitadinathsalgar@dokonaly.com>
2024-01-30 18:14:07 +0530

    profile schedule changes

--- src/components/ProfileScheduleList/PostTitle.jsx
+++ src/components/ProfileScheduleList/PostTitle.jsx
@@ -1,39 +1,44 @@
-import { PersonAddOutlined } from "@mui/icons-material";
 import { Box, IconButton, Typography, useTheme } from "@mui/material";
-import FlexBetween from "..//FlexBetween";
-import Avatar from '@mui/material/Avatar';
+import Avatar from "@mui/material/Avatar";
 import { useSelector } from "react-redux";
-import {
-  DeleteOutlined,
-} from "@mui/icons-material";
-import { useDeletePost } from "../../hooks/posts";
-import { useGetAllSchedules } from "../../hooks/schedule";
-import moment from "moment"
+import { DeleteOutlined } from "@mui/icons-material";
+import moment from "moment";
 import { AiOutlineUsergroupAdd } from "react-icons/ai";
-import { useGetProfile } from "../../hooks/profile";
+import FlexBetween from "../FlexBetween";
+import Loader from "../Loader/Loader";
+import { useDeleteSchedule } from "../../hooks/schedule";
+
 const PostTitle = ({ data }) => {
   const { palette } = useTheme();
   const primaryLight = palette.primary.light;
   const primaryDark = palette.primary.dark;
   const main = palette.neutral.main;
   const medium = palette.neutral.medium;
-  const { userId } = useSelector((state) => state.profile.profileData)
-  const { profileData} = useGetProfile(userId);
+  const companyId = useSelector((state) => state.profile.companyId);
 
-  const { mutate, isLoading } = useDeletePost()
+  const { mutate, isLoading } = useDeleteSchedule();
   const deletePost = (id) => {
     const postData = {
-      postId: id,
-      userId
-    }
-    mutate(postData)
+      scheduleId: id,
+      companyId: companyId,
+    };
+    mutate(postData);
+  };
+  if (isLoading) {
+    <Loader />;
   }
+
+  console.log(data , 'dataaasdass')
 
   return (
     <FlexBetween>
-      <FlexBetween gap="1rem">
-        <Avatar sx={{ width: 35, height: 35  }} alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-        <Box onClick={() => { }}>
+      <FlexBetween gap="10px">
+        <Avatar
+          sx={{ width: 35, height: 35 }}
+          alt="Remy Sharp"
+          src="/static/images/avatar/1.jpg"
+        />
+        <Box onClick={() => {}}>
           <Typography
             color={main}
             variant="h5"
@@ -48,19 +53,18 @@ const PostTitle = ({ data }) => {
           </Typography>
         </Box>
         <Typography color={medium} fontSize="0.75rem">
-            {moment(data?.createdAt).format('MMM Do YYYY, h:mm a')}
-          </Typography>
+          {moment(data?.createdAt).format("MMM Do YYYY, h:mm a")}
+        </Typography>
       </FlexBetween>
-      {data?.companyId != profileData?.pageDate?.companyId
-        ?
+      {data?.companyId !== companyId ? (
         <IconButton sx={{ backgroundColor: primaryLight, p: "0.6rem" }}>
           <AiOutlineUsergroupAdd sx={{ color: primaryDark }} />
-        </IconButton> :
+        </IconButton>
+      ) : (
         <IconButton sx={{ p: "0.6rem" }} onClick={() => deletePost(data?._id)}>
           <DeleteOutlined />
-
         </IconButton>
-      }
+      )}
     </FlexBetween>
   );
 };
--- src/hooks/posts.jsx
+++ src/hooks/posts.jsx
@@ -173,6 +173,7 @@ const useDeletePost = () => {
       queryClient.invalidateQueries({ queryKey: ["trending"] });
       queryClient.invalidateQueries({ queryKey: ["forYou"] });
       queryClient.invalidateQueries({ queryKey: ["friend"] });
+      queryClient.invalidateQueries({ queryKey: ["postList"] });
     },
     onError: (error) => {
       toast.error(error.message.split(":")[1]);
