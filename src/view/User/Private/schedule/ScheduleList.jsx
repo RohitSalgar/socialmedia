8481c001a11d2edf406288dc48af0673335d9be8
vishnupriyaa0505 <vp.vishnupriya@dokonaly.com>
2024-02-06 18:47:50 +0530

    schedulepagenation

--- src/view/User/Private/schedule/ScheduleList.jsx
+++ src/view/User/Private/schedule/ScheduleList.jsx
@@ -7,64 +7,106 @@ import Loader from "../../../../components/Loader/Loader";
 import moment from "moment";
 import PostTitle from "./PostTitle";
 import LookingEmpty from "../../../../components/LookingEmpty/LookingEmpty";
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+import { useSelector } from "react-redux";
+import PostSkeleton from "../../../../components/Skeleton/PostSkeleton";
 
 const ScheduleList = () => {
-  const { data, isLoading } = useGetAllSchedules();
+  const { ref, inView } = useInView();
+  const {hashtag} = useSelector((state)=>state.post)
+  const dashboardView = useSelector((state) => state.profile.dashboardView);
+  const {
+    data,
+    isLoading,
+    isFetchingNextPage: scheduleFetchingNextPage,
+    error: scheduleError,
+    isFetching: scheduleFetching,
+    fetchNextPage: scheduleFetchNextPage,
+    hasNextPage: scheduleHasNextPage,
+  } = useGetAllSchedules();
+
+  useEffect(() => {
+    if (inView) {
+      if (
+        hashtag === "" &&
+        dashboardView === "schedule" &&
+        !data?.pageParams.includes(Math.ceil(data?.pages[0]?.totalCount / 5))
+      ) {
+        scheduleFetchNextPage();
+      }
+    }
+  }, [inView]);
+
   if (isLoading) {
     return <Loader />;
   }
 
   return (
     <>
-      {data && data.length > 0 ? (
-        <>
-          {data &&
-            data.map((e, i) => {
-              return (
-                <WidgetWrapper key={i} sx={{ marginBottom: "10px" }}>
-                  <PostTitle data={e} />
-                  <Box className={styles.scheduleListDiv}>
-                    <Box>
-                      <Box className={styles.scheduleListSubDiv}>
-                        <Typography>POL: </Typography>{" "}
-                        <Typography>{e.pol}</Typography>
-                      </Box>
-                      <Box className={styles.scheduleListSubDiv}>
-                        <Typography>POD: </Typography>{" "}
-                        <Typography>{e.pod}</Typography>
-                      </Box>
-                    </Box>
-                    <Box>
-                    <Box className={styles.scheduleListSubDiv}>
-                        <Typography>Opening On: </Typography>
-                        <Typography>
-                          {moment(e.openingOn).format("DD-MM-YYYY")}
-                        </Typography>
-                      </Box>
-                      
-                      <Box className={styles.scheduleListSubDiv}>
-                        <Typography>Booking Cut-Off: </Typography>
-                        <Typography>
-                          {moment(e.bookingCutOff).format("DD-MM-YYYY")}
-                        </Typography>
-                      </Box>
-                    </Box>
+      {data && data?.pages && data.pages.length > 0 ? (
+        data.pages.map(({ data }) => {
+          return data.map((e, i) => (
+            <WidgetWrapper key={i} sx={{ marginBottom: "10px" }}>
+              <PostTitle data={e} />
+              <Box className={styles.scheduleListDiv}>
+                <Box>
+                  <Box className={styles.scheduleListSubDiv}>
+                    <Typography>POL: </Typography>
+                    <Typography>{e.pol}</Typography>
                   </Box>
-                  {e.description !== "" && (
-                    <Box className={styles.descriptiondiv}>
-                      <p>{e.description} </p>
-                    </Box>
-                )}
-                  <Box>
-                    <LikeComment key={e._id} postData={e} scheduleId={e._id} />
+                  <Box className={styles.scheduleListSubDiv}>
+                    <Typography>POD: </Typography>
+                    <Typography>{e.pod}</Typography>
                   </Box>
-                </WidgetWrapper>
-              );
-            })}
-        </>
+                </Box>
+                <Box>
+                  <Box className={styles.scheduleListSubDiv}>
+                    <Typography>Opening On: </Typography>
+                    <Typography>
+                      {moment(e.openingOn).format("DD-MM-YYYY")}
+                    </Typography>
+                  </Box>
+                  <Box className={styles.scheduleListSubDiv}>
+                    <Typography>Booking Cut-Off: </Typography>
+                    <Typography>
+                      {moment(e.bookingCutOff).format("DD-MM-YYYY")}
+                    </Typography>
+                  </Box>
+                </Box>
+              </Box>
+              {e.description !== "" && (
+                <Box className={styles.descriptiondiv}>
+                  <p>{e.description}</p>
+                </Box>
+              )}
+              <Box>
+                <LikeComment key={e._id} postData={e} scheduleId={e._id} />
+              </Box>
+            </WidgetWrapper>
+          ));
+        })
       ) : (
-        <LookingEmpty/>
+        <div style={{ marginTop: "10px" }}>
+          <LookingEmpty />
+        </div>
       )}
+
+      {!data?.pageParams.includes(
+        Math.ceil(data?.pages[0]?.totalCount / 5)
+      ) && <PostSkeleton />}
+      <div
+        ref={ref}
+        style={{ height: "10px" }}
+        onClick={() => scheduleFetchNextPage()}
+      >
+        {scheduleFetchingNextPage && <PostSkeleton />}
+      </div>
+      {/* {(!data || !data.pages || data.pages.length === 0) && (
+        <div style={{ marginTop: "10px" }}>
+          <LookingEmpty />
+        </div>
+      )} */}
     </>
   );
 };
